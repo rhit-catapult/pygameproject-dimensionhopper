3f5e20aa1f6ce783f336050b4848577045dd91fb
Askalagas1 <arthur.tth.2024270@gmail.com>
2024-07-15 15:46:34 -0400

    arthur commit background change

--- project.py
+++ project.py
@@ -15,6 +15,33 @@ def change_background():
     current_background_index = (current_background_index + 1) % len(backgrounds)
 
 
+class Spike:
+    def __init__(self, screen, x, y, image_filename):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(image_filename)
+
+    #Spike is 60#83
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
+
+class Block:
+    def __init__(self, screen, x, y, width, height, color):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(x, y, width, height)
+
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
+
+
 def main():
     # turn on pygame
     pygame.init()
@@ -22,7 +49,7 @@ def main():
     # create a screen
     pygame.display.set_caption("Dimension Hopper")
     # TODO: Change the size of the screen as you see fit!
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((1400, 860))
 
     # let's set the framerate
     clock = pygame.time.Clock()
